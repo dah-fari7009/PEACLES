32937b21e309ff11c77db5c9ad2c7cac04f59b11
Faridah Akinotcho <faridath.akinotcho@yahoo.fr>
2019-04-25 12:43:49 +0200

    user

--- PEACLES/src/Form/UserType.php
+++ PEACLES/src/Form/UserType.php
@@ -6,19 +6,25 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\TextType;
+use Symfony\Component\Form\Extension\Core\PasswordType;
+use Symfony\Component\Form\Extension\Core\EmailType;
+use Symfony\Component\Form\Extension\Core\TextareaType;
+use Symfony\Component\Form\Extension\Core\DateType;
+use Symfony\Component\Form\Extension\Core\IntegerType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('pwd')
-            ->add('bday')
-            ->add('phone')
-            ->add('bio')
-            ->add('profile_pic')
+            ->add('name',TextType::class)
+            ->add('email',EmailType::class)
+            ->add('pwd',PasswordType::class)
+            ->add('bday',DateType::class)
+            ->add('phone',IntegerType::class)
+            ->add('bio',TextareaType::class)
+            ->add('profile_pic',TextType::class)
         ;
     }
 
