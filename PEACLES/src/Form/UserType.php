3f37a81c6d5511bd250d8a95a6cb679884ff287b
Faridah Akinotcho <faridath.akinotcho@yahoo.fr>
2019-04-25 11:59:26 +0200

    nooooooooooo

---
+++ PEACLES/src/Form/UserType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('pwd')
+            ->add('bday')
+            ->add('phone')
+            ->add('bio')
+            ->add('profile_pic')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
